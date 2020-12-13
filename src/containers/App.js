d6919cd0a1773bc4e3f5c5eeebb918761b3aded2
hutor04 <yauhen.khutarniuk@gmail.com>
2020-12-13 22:41:36 +0100

    Init commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import './App.css';
+import logo from '../logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo}/>
+    </div>
+  );
+}
+
+export default App;
